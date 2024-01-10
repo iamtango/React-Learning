f92868db56cddf83cb812573fe2fe48a8602b93e
iamtango <iamtango29@gmail.com>
2024-01-10 08:21:31 +0530

    Machine code interview practice

--- 13.Machine Coding Interview/src/components/Body.jsx
+++ 13.Machine Coding Interview/src/components/Body.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Sidebar from "./Sidebar";
-import MainComponent from "./MainComponent";
 import { Outlet } from "react-router-dom";
 
 const Body = () => {
--- 13.Machine Coding Interview/src/utils/consant.jsx
+++ 13.Machine Coding Interview/src/utils/consant.jsx
@@ -1,4 +1,4 @@
-const API_Key = "AIzaSyB5CuEDVbZbc0QNTvEWXfXGw2xzB9e0l2I";
+const API_Key = "AIzaSyDVRcLwv4bCS8w2lPkiyjCoW9cAp7s4sDQ";
 
 export const Youtube_API =
   "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
