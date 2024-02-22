d72bfc15ede0ba6b1a4567923755de504f8ed923
iamtango <iamtango29@gmail.com>
2024-02-22 20:16:58 +0530

    Machine Coding Interview

---
+++ 13.Machine Coding Interview/src/utils/constant.jsx
@@ -0,0 +1,8 @@
+const API_Key = "AIzaSyDVRcLwv4bCS8w2lPkiyjCoW9cAp7s4sDQ";
+
+export const Youtube_API =
+  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
+  API_Key;
+
+export const Youtube_Search_API =
+  "https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
