b8aed5dc30a1c4744c95f24f25495b5aa0ccfdbd
iamtango <iamtango29@gmail.com>
2024-05-13 22:01:25 +0530

    Moonlight Restaurant App

---
+++ MoonLight/src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+// Footer component for footer section
+
+const Footer = () => {
+  return (
+    <footer className="bg-gray-800 text-white py-4  bottom-0 w-full">
+      <div className="container mx-auto flex justify-center items-center">
+        <span className="flex items-center">
+          <p className="mr-2">Created with 💖</p>
+
+          <p>by Vedang Nikure</p>
+        </span>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
